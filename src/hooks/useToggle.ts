4e7ce95ff159ef61b7724cff7c8d5093bfcc3af8
Tsapov-Maxim <tsapov552@gmail.com>
2021-07-31 22:35:04 +0700

    Added useToggle hook

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export const useToggle = (intialState = false) => {
+  const [isOpen, setIsOpen] = useState(intialState);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return [isOpen, handleOpen];
+};
